5ab289a829e7152520ff4d7d5a0d0eb879a26254
Hung304-WBLEM <hungvq.blem.304@gmail.com>
2026-02-05 16:59:50 -0600

    update

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,5 +1,11 @@
+# python -m torch.distributed.launch --nproc_per_node=2 --master_port=29516 eval.py \
+#     --config configs/TransCP_R50_pathology2.py \
+#     --test_split testB \
+#     --resume checkpoint/checkpoint_best_acc.pth \
+#     --split_root split/data
+
 python -m torch.distributed.launch --nproc_per_node=2 --master_port=29516 eval.py \
     --config configs/TransCP_R50_pathology2.py \
-    --test_split testB \
+    --test_split testA \
     --resume checkpoint/checkpoint_best_acc.pth \
     --split_root split/data
