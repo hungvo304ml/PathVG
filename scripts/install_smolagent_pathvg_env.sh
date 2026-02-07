18ae9249adfac0bc1d34da0b75f498ea1c5ca0e0
Hung304-WBLEM <hungvq.blem.304@gmail.com>
2026-02-07 11:57:46 -0600

    update

--- scripts/install_smolagent_pathvg_env.sh
+++ scripts/install_smolagent_pathvg_env.sh
@@ -1,7 +1,11 @@
 conda create -n smolagent_pathvg_env python=3.10 -y
 conda activate smolagent_pathvg_env
 
-conda install nvidia::cuda-toolkit==11.8.0 -y
+conda install nvidia::cuda-toolkit==12.4.0 -y
 pip install -U sympy==1.13.1
-conda install pytorch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1  pytorch-cuda=11.8 -c pytorch -c nvidia -y
+conda install pytorch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 pytorch-cuda=12.4 -c pytorch -c nvidia
 pip install smolagents
+pip install ipykernel
+pip install mkl==2024.0.0
+pip install 'smolagents[openai]'
+pip install scipy opencv-python shapely micropip scikit-learn imageio seaborn scikit-image matplotlib
